e02ac872183c6978bb648254b35247a5f2793f8f
ponomarovs87 <ponomarovs87@gmail.com>
2023-09-25 13:05:06 +0200

    del wordpress project

--- wordpress-site/xampp/cgi-bin/cgi.cgi
+++
@@ -1,5 +0,0 @@
-#!"C:\homework\JS_Lessons\wordpress-site\xampp\perl\bin\perl.exe"
-
-print "Content-Type: text/html\n\n";
-
-print "OK";
--- wordpress-site/xampp/htdocs/mywebsite/wp-content/plugins/ocean-extra/includes/preloader/assets/js/preloader.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load',  async function () {
-
-   document.body.classList.remove('ocean-preloader--active');
-
-   const element = document.getElementById('ocean-preloader');
-    element.remove();
-});
